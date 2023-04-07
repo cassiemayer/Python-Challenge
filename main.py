8193559ac8d9af42e5ad080f2b06916b3d2bf81e
cassiemayer <123522580+cassiemayer@users.noreply.github.com>
2023-04-07 11:58:12 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,40 @@
 import os
 import csv
 
-input_file = ("python-challenge", "PyBank", "budget_data.csv")
-# Create empty lists to iterate through specific rows for the following variables
-total_months = []
-total_profit = []
-monthly_profit_change = []
+input_file = ('python-challenge', 'PyPoll', 'election_data.csv')
 
-csvpath = "./Resources/budget_data.csv"
+total_votes_casted = []
+candidates_who_ran = { "CandidateOne" : "Charles Casper Stockham",
+                      "CandidateTwo" : "Dianna Deggette",
+                      "CandidateThree" : "Raymon Anthony Doane"}
+candidate_who_won = {"Dianna Deggette" : "Winner"}
+
+csvpath = './Resources/election_data.csv'
 
 with open(csvpath, "r") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ",")
-    # Skip the header labels to iterate with the values
-    header = next(csvreader)
-    
+        csvreader = csv.reader(csvfile, delimiter = ",")
+        header = next(csvreader)
+        for row in csvreader: 
+                total_votes_casted.append(int(row[0]))
+for candidates in candidates_who_ran:
+        total_votes = len(total_votes_casted)
 
-    for row in csvreader:
-        total_months.append(row[0])
-        total_profit.append(int(row[1]))
 
-    for i in range(len(total_profit)-1):
-        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
 
-# Obtain the max and min of the the montly profit change list
-max_increase_value = max(monthly_profit_change)
-max_decrease_value = min(monthly_profit_change)
-# Correlate max and min to the proper month using month list and index from max and min
-#We use the plus 1 at the end since month associated with change is the + 1 month or next month
-max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
-
-#Print Statements
-# Print Statements
-
-print("Financial Analysis")
+print("Election Results")
 print("---")
-print(f"Total Months: {len(total_months)}")
-print(f"Total: ${sum(total_profit)}")
-print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
-print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+print(f"total_votes: {total_votes}")
+print(f"values({candidates_who_ran})")
+print(candidate_who_won)
 
-#Output files
-# Output files
-output_file = "./Analysis/budget_analysis.txt"
+output_file = "./Analysis/election_data.txt"
 
 with open(output_file,"w") as file:
-#Write methods to print to Financial_Analysis_Summary
-
-# Write methods to print to Financial_Analysis_Summary
-    file.write("Financial Analysis")
-    file.write("\n")
-    file.write("---")
-    file.write("\n")
-    file.write(f"Total Months: {len(total_months)}")
-    file.write("\n")
-    file.write(f"Total: ${sum(total_profit)}")
-    file.write("\n")
-    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-    file.write("\n")
-    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
-    file.write("\n")
-    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
-
+        file.write("Election Results")
+        file.write("\n")
+        file.write("---")
+        file.write("\n")
+        file.write(f"Total Votes: {total_votes}")
+        file.write("\n")
+        file.write(f"{candidates}")
+        file.write(f'{candidate_who_won}')
